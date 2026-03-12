13321fcdf4d368f3b9cc8de0100b6402e066984c
danielepucci03 <danielepucci03@gmail.com>
2026-03-12 16:40:43 +0100

    create discount & conditional statements

--- main.js
+++ main.js
@@ -2,6 +2,13 @@
 const km = prompt('quanti km devi percorrere?', 'es. 150');
 const age = prompt('quanti anni hai?', 'es,13');
 
+//calcoliamo il prezzo del biglietto
+const price = km * 0.21;
+let discount = 0;
 
-
+if (age < 18) {
+    discount = price * 0.2;
+} else if (age > 65) {
+    discount = price * 0.4;
+}
 
